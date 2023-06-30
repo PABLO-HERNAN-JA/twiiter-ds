86273e70ebd7fdddad3f3c9711b3780f595be9a6
pablo jasso <hernanja.tij20@utsjr.edu.mx>
2023-06-30 17:33:53 -0600

    FINAL

--- social_project/settings.py
+++ social_project/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'social_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dcd1q191ctn3q9',
+        'DATABASE': 'dcd1q191ctn3q9',
         'USER': 'geetsfwttznzkn',
         'PASSWORD': 'd60c857a56debb6cb2847e46ed673681350c36ebf40f1bcdebac2db726c4b006',
         'HOST': 'ec2-3-233-77-220.compute-1.amazonaws.com',
